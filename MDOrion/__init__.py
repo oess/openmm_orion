2a4cac4ebf0b413e3b6321adc22d7041018a8d3f
Gaetano Calabró <gcalabro@eyesopen.com>
2019-07-01 14:14:17 -0600

    Change version number for release to 0.9.2

--- MDOrion/__init__.py
+++ MDOrion/__init__.py
@@ -15,7 +15,7 @@
 # liable for any damages or liability in connection with the Sample Code
 # or its use.
 
-__version__ = '0.9.1b2'
+__version__ = '0.9.2'
 
 
 from .ComplexPrep.cubes import ComplexPrepCube
