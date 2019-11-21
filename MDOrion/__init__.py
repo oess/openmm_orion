6d58831b7830ab78442659db22c89ac5a7bad1e4
Gaetano Calabro <gcalabro@eyesopen.com>
2019-11-21 10:17:32 -0700

    Release candidate

--- MDOrion/__init__.py
+++ MDOrion/__init__.py
@@ -15,7 +15,7 @@
 # liable for any damages or liability in connection with the Sample Code
 # or its use.
 
-__version__ = '0.9.5b26'
+__version__ = '0.9.5'
 
 
 from .ComplexPrep.cubes import ComplexPrepCube
