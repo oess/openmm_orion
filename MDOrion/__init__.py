8a0d980a69d845adf6024b4c71845e08f7dafe1c
Gaetano Calabro <gcalabro@eyesopen.com>
2019-12-02 15:48:58 -0700

    Update pkg version

--- MDOrion/__init__.py
+++ MDOrion/__init__.py
@@ -15,7 +15,7 @@
 # liable for any damages or liability in connection with the Sample Code
 # or its use.
 
-__version__ = '0.9.5b30'
+__version__ = '0.9.5b32'
 
 
 from .ComplexPrep.cubes import ComplexPrepCube
