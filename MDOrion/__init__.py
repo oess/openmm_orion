93662d6e588b246b33a222a8c26c12c509dec681
Gaetano Calabro <gcalabro@eyesopen.com>
2020-03-19 09:18:36 -0600

    Bump version

--- MDOrion/__init__.py
+++ MDOrion/__init__.py
@@ -15,7 +15,7 @@
 # liable for any damages or liability in connection with the Sample Code
 # or its use.
 
-__version__ = '0.9.7b68'
+__version__ = '0.9.7b70'
 
 
 from .ComplexPrep.cubes import ComplexPrepCube
