5da7cf3a9cddf6502961c3dcf4dff1a364aa572b
Gaetano Calabro <gcalabro@eyesopen.com>
2020-04-08 17:04:24 -0600

    Update to release final version

--- MDOrion/__init__.py
+++ MDOrion/__init__.py
@@ -15,7 +15,7 @@
 # liable for any damages or liability in connection with the Sample Code
 # or its use.
 
-__version__ = '1.0.0rc4'
+__version__ = '1.0.0'
 
 from .ComplexPrep.cubes import ComplexPrepCube
 
