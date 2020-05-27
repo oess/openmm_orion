35d5e698fbaadf5e5b78eb7560b5817d75ebeeb6
Christopher Bayly <bayly@eyesopen.com>
2020-05-27 17:14:55 -0600

    Revved version to 2.0.0a9

--- MDOrion/__init__.py
+++ MDOrion/__init__.py
@@ -15,7 +15,7 @@
 # liable for any damages or liability in connection with the Sample Code
 # or its use.
 
-__version__ = '2.0.0a7'
+__version__ = '2.0.0a9'
 
 from .ComplexPrep.cubes import ComplexPrepCube
 
