b860f45c50a7fa6519a67bdb6f9005d676d49cdb
Christopher Bayly <bayly@eyesopen.com>
2020-06-24 16:12:39 -0600

    Modified floe ProteinLigandMD.py to correspond to the MD front end of ShortTrajMDWithAnalysis.py

--- floes/ProteinLigandMD.py
+++ floes/ProteinLigandMD.py
@@ -75,16 +75,9 @@ complx = ComplexPrepCube("Complex", title="Complex Preparation")
 
 # The solvation cube is used to solvate the system and define the ionic strength of the solution
 solvate = ParallelSolvationCube("Solvation", title="Solvation")
-solvate.modify_parameter(solvate.density, promoted=False, default=1.03)
-#                           description="Solution density in g/ml")
-solvate.modify_parameter(solvate.salt_concentration, promoted=False, default=50.0)
-#                           description='Salt concentration (Na+, Cl-) in millimolar')
-solvate.set_parameters(density=1.03)
-solvate.set_parameters(salt_concentration=50.0)
-solvate.modify_parameter(solvate.close_solvent, promoted=False, default=False)
 
 # This cube is necessary for the correct work of collection and shard
-coll_open = CollectionSetting("OpenCollection")
+coll_open = CollectionSetting("OpenCollection", title="Open Collection")
 coll_open.set_parameters(open=True)
 
 # Force Field Application
@@ -96,14 +89,17 @@ ff.promote_parameter('ligand_forcefield', promoted_name='ligand_ff', default='Op
 mdcomp = MDComponentCube("MD Components", title="MD Components")
 mdcomp.promote_parameter("flask_title", promoted_name="flask_title", default="")
 
+
+# Production run
 prod = ParallelMDNptCube("Production", title="Production")
 prod.promote_parameter('time', promoted_name='prod_ns', default=2.0,
                        description='Length of MD run in nanoseconds')
 prod.promote_parameter('trajectory_interval', promoted_name='prod_trajectory_interval', default=0.004,
                        description='Trajectory saving interval in ns')
-prod.promote_parameter('hmr', title='Use Hydrogen Mass Repartitioning', default=True,
+prod.promote_parameter('hmr', promoted_name="HMR", title='Use Hydrogen Mass Repartitioning', default=True,
                        description='Give hydrogens more mass to speed up the MD')
-prod.set_parameters(md_engine='OpenMM')
+prod.promote_parameter('md_engine', promoted_name='md_engine', default='OpenMM',
+                       description='Select the MD Engine')
 prod.set_parameters(reporter_interval=0.004)
 prod.set_parameters(suffix='prod')
 
@@ -116,7 +112,7 @@ minComplex.set_parameters(steps=0)
 minComplex.set_parameters(center=True)
 minComplex.set_parameters(save_md_stage=True)
 minComplex.set_parameters(hmr=False)
-minComplex.set_parameters(md_engine='OpenMM')
+minComplex.promote_parameter("md_engine", promoted_name="md_engine")
 
 # NVT simulation. Here the assembled system is warmed up to the final selected temperature
 warmup = ParallelMDNvtCube('warmup', title='Warm Up')
@@ -126,61 +122,60 @@ warmup.modify_parameter(warmup.restraintWt, promoted=False, default=2.0)
 warmup.set_parameters(trajectory_interval=0.0)
 warmup.set_parameters(reporter_interval=0.001)
 warmup.set_parameters(suffix='warmup')
-# warmup.promote_parameter("hmr", promoted_name="hmr")
 warmup.set_parameters(hmr=False)
 warmup.set_parameters(save_md_stage=True)
-warmup.set_parameters(md_engine='OpenMM')
+warmup.promote_parameter("md_engine", promoted_name="md_engine")
 
 
-# The system is equilibrated at the desired pressure and temperature in several stages
-# The main difference between the stages is pregressively weakening the restraint force used
-# to keep the ligand and protein in their starting positions.
+# The system is equilibrated at the right pressure and temperature in several stages
+# The main difference between the stages is related to the restraint force used
+# to keep the ligand and protein in their starting positions. A relatively strong force
+# is applied in the first stage while a relatively small one is applied in the latter
 
 # NPT Equilibration stage 1
 equil1 = ParallelMDNptCube('equil1', title='Equilibration I')
 equil1.set_parameters(time=0.01)
-equil1.set_parameters(hmr=False)
+equil1.promote_parameter("hmr", promoted_name="HMR")
 equil1.modify_parameter(equil1.restraints, promoted=False, default="noh (ligand or protein)")
 equil1.modify_parameter(equil1.restraintWt, promoted=False, default=1.0)
 equil1.set_parameters(trajectory_interval=0.0)
 equil1.set_parameters(reporter_interval=0.001)
 equil1.set_parameters(suffix='equil1')
-equil1.set_parameters(md_engine='OpenMM')
+equil1.promote_parameter("md_engine", promoted_name="md_engine")
 
 
 # NPT Equilibration stage 2
 equil2 = ParallelMDNptCube('equil2', title='Equilibration II')
 equil2.set_parameters(time=0.02)
-equil2.set_parameters(hmr=True)
+equil2.promote_parameter("hmr", promoted_name="HMR")
 equil2.modify_parameter(equil2.restraints, promoted=False, default="noh (ligand or protein)")
 equil2.modify_parameter(equil2.restraintWt, promoted=False, default=0.5)
 equil2.set_parameters(trajectory_interval=0.0)
 equil2.set_parameters(reporter_interval=0.001)
 equil2.set_parameters(suffix='equil2')
-equil2.set_parameters(md_engine='OpenMM')
+equil2.promote_parameter("md_engine", promoted_name="md_engine")
 
 # NPT Equilibration stage 3
 equil3 = ParallelMDNptCube('equil3', title='Equilibration III')
 equil3.modify_parameter(equil3.time, promoted=False, default=0.1)
-equil3.set_parameters(hmr=True)
-# equil3.modify_parameter(equil3.restraints, promoted=False, default="ca_protein or (noh ligand)")
+equil3.promote_parameter("hmr", promoted_name="HMR")
 equil3.modify_parameter(equil3.restraints, promoted=False, default="noh (ligand or protein)")
 equil3.modify_parameter(equil3.restraintWt, promoted=False, default=0.2)
 equil3.set_parameters(trajectory_interval=0.0)
 equil3.set_parameters(reporter_interval=0.002)
 equil3.set_parameters(suffix='equil3')
-equil3.set_parameters(md_engine='OpenMM')
+equil3.promote_parameter("md_engine", promoted_name="md_engine")
 
 # NPT Equilibration stage 4
 equil4 = ParallelMDNptCube('equil4', title='Equilibration IV')
 equil4.modify_parameter(equil4.time, promoted=False, default=0.1)
-equil4.set_parameters(hmr=True)
+equil4.promote_parameter("hmr", promoted_name="HMR")
 equil4.modify_parameter(equil4.restraints, promoted=False, default="ca_protein or (noh ligand)")
 equil4.modify_parameter(equil4.restraintWt, promoted=False, default=0.1)
 equil4.set_parameters(trajectory_interval=0.0)
 equil4.set_parameters(reporter_interval=0.002)
 equil4.set_parameters(suffix='equil4')
-equil4.set_parameters(md_engine='OpenMM')
+equil4.promote_parameter("md_engine", promoted_name="md_engine")
 
 md_group = ParallelCubeGroup(cubes=[minComplex, warmup, equil1, equil2, equil3, equil4, prod])
 job.add_group(md_group)
@@ -194,7 +189,7 @@ fail.promote_parameter("data_out", promoted_name="fail", title="Failures",
                        description="MD Dataset Failures out")
 
 # This cube is necessary for the correct working of collection and shard
-coll_close = CollectionSetting("CloseCollection")
+coll_close = CollectionSetting("CloseCollection", title="Close Collection")
 coll_close.set_parameters(open=False)
 
 check_rec = ParallelRecordSizeCheck("Record Check Success")
@@ -204,6 +199,7 @@ job.add_cubes(iligs, ligset, iprot, mdcomp, chargelig, complx,
               minComplex, warmup, equil1, equil2, equil3, equil4, prod,
               coll_close, check_rec, ofs, fail)
 
+# Success Connections
 iligs.success.connect(ligset.intake)
 ligset.success.connect(chargelig.intake)
 chargelig.success.connect(ligid.intake)
@@ -213,7 +209,6 @@ mdcomp.success.connect(complx.protein_port)
 complx.success.connect(solvate.intake)
 solvate.success.connect(coll_open.intake)
 coll_open.success.connect(ff.intake)
-coll_open.failure.connect(check_rec.fail_in)
 ff.success.connect(minComplex.intake)
 minComplex.success.connect(warmup.intake)
 warmup.success.connect(equil1.intake)
@@ -222,11 +217,28 @@ equil2.success.connect(equil3.intake)
 equil3.success.connect(equil4.intake)
 equil4.success.connect(prod.intake)
 prod.success.connect(coll_close.intake)
-prod.failure.connect(check_rec.fail_in)
 coll_close.success.connect(check_rec.intake)
-coll_close.failure.connect(check_rec.fail_in)
 check_rec.success.connect(ofs.intake)
+
+# Fail Connections
+ligset.failure.connect(check_rec.fail_in)
+chargelig.failure.connect(check_rec.fail_in)
+ligid.failure.connect(check_rec.fail_in)
+mdcomp.failure.connect(check_rec.fail_in)
+complx.failure.connect(check_rec.fail_in)
+solvate.failure.connect(check_rec.fail_in)
+coll_open.failure.connect(check_rec.fail_in)
+ff.failure.connect(check_rec.fail_in)
+minComplex.failure.connect(check_rec.fail_in)
+warmup.failure.connect(check_rec.fail_in)
+equil1.failure.connect(check_rec.fail_in)
+equil2.failure.connect(check_rec.fail_in)
+equil3.failure.connect(check_rec.fail_in)
+equil4.failure.connect(check_rec.fail_in)
+prod.failure.connect(check_rec.fail_in)
+coll_close.failure.connect(check_rec.fail_in)
 check_rec.failure.connect(fail.intake)
+
 
 if __name__ == "__main__":
     job.run()
