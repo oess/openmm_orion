5bf0dddc5e302da7dbc191a5df08cfee01251953
Gaetano Calabro <gcalabro@eyesopen.com>
2020-06-30 09:15:23 -0600

    Added py3.7.7 support

--- MDOrion/__init__.py
+++ MDOrion/__init__.py
@@ -15,7 +15,7 @@
 # liable for any damages or liability in connection with the Sample Code
 # or its use.
 
-__version__ = '2.0.0b24'
+__version__ = '2.0.0b26'
 
 from .ComplexPrep.cubes import ComplexPrepCube
 
