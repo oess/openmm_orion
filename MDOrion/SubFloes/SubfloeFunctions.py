b79ef8e9a71608a6e33421725b11634ec11485e1
Gaetano Calabro <gcalabro@eyesopen.com>
2021-04-23 14:04:00 -0600

    NES gpu/cpu settings

--- MDOrion/SubFloes/SubfloeFunctions.py
+++ MDOrion/SubFloes/SubfloeFunctions.py
@@ -87,20 +87,23 @@ def setup_NonEquilSwch_GMX(input_floe, input_bound, input_unbound, check_rec, op
     unbound_nes = ParallelNESGMX("GMXUnboundNES", title="GMX Unbound NES")
     unbound_nes.promote_parameter("time", promoted_name="nes_time",
                                   default=options['nes_switch_time_in_ns'], order=3)
+
     unbound_nes.modify_parameter(unbound_nes.instance_type, promoted=False, default='c5')
     unbound_nes.modify_parameter(unbound_nes.cpu_count, promoted=False, default=2)
     unbound_nes.modify_parameter(unbound_nes.gpu_count, promoted=False, default=0)
-    unbound_nes.modify_parameter(unbound_nes.memory_mb, promoted=False, default=4*1024)
+    unbound_nes.modify_parameter(unbound_nes.memory_mb, promoted=False, default=3*1024)
     unbound_nes.modify_parameter(unbound_nes.gmx_mpi_threads, promoted=False, default=1)
     unbound_nes.modify_parameter(unbound_nes.gmx_openmp_threads, promoted=False, default=2)
+    unbound_nes.modify_parameter(unbound_nes.max_parallel, promoted=False, default=10000)
 
     bound_nes = ParallelNESGMX("GMXBoundNES", title="GMX Bound NES")
     bound_nes.promote_parameter("time", promoted_name="nes_time", order=3)
-    bound_nes.modify_parameter(bound_nes.instance_type, promoted=False, default='g3')
+    # bound_nes.modify_parameter(bound_nes.instance_type, promoted=False, default='g3')
     bound_nes.modify_parameter(bound_nes.cpu_count, promoted=False, default=16)
     bound_nes.modify_parameter(bound_nes.gpu_count, promoted=False, default=1)
     bound_nes.modify_parameter(bound_nes.gmx_mpi_threads, promoted=False, default=1)
     bound_nes.modify_parameter(bound_nes.gmx_openmp_threads, promoted=False, default=16)
+    bound_nes.modify_parameter(bound_nes.max_parallel, promoted=False, default=10000)
 
     nes_analysis = NESAnalysis("NES_Analysis")
 
