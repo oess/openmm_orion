ef5b52337939d058cf43e59423dbdf61b9271d7b
Gaetano Calabro <gcalabro@eyesopen.com>
2021-05-06 17:21:25 -0600

    new pip version

--- setup.py
+++ setup.py
@@ -24,14 +24,11 @@ from setuptools import setup, find_packages
 
 from pip._internal.req import parse_requirements
 
-from pip._internal.download import PipSession
-
-
 def get_reqs(reqs):
-    return [str(ir.req) for ir in reqs]
+    return [str(ir.requirement) for ir in reqs]
 
 
-install_reqs = get_reqs(parse_requirements("requirements_dev.txt", session=PipSession()))
+install_reqs = get_reqs(parse_requirements("requirements_dev.txt", session=None))
 
 
 def get_version():
